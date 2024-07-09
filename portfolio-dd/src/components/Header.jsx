15600a92452f666a7e54c87c6e2c9f3f5c106b3d
Jashanpreet Singh <141000921+Chapmo64@users.noreply.github.com>
2024-07-09 14:21:29 +0530

    navbar-01

--- portfolio-dd/src/components/Header.jsx
+++ portfolio-dd/src/components/Header.jsx
@@ -1,10 +1,103 @@
-import React from 'react'
+import React from "react";
 
 export const Header = () => {
   return (
     <header>
-        <h1>Run</h1>
-        <img src="i8-lunacy://node?id=CdQ5pqyG_02D3tfY6gB0zw" alt="f" />
+      {/* <nav className="navbar bg-body-tertiary">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img
+              src="/docs/5.3/assets/brand/bootstrap-logo.svg"
+              alt="Bootstrap"
+              width={30}
+              height={24}
+            />
+          </a>
+        </div>
+        <div classname="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                HOME
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                ABOUT
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                SERVICES
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                PORTFOLIO
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                CONTACT
+              </a>
+            </li>
+          </ul>
+          
+        </div>
+      </nav> */}
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img
+              src="/docs/5.3/assets/brand/bootstrap-logo.svg"
+              alt="Bootstrap"
+              width={30}
+              height={24}
+            />
+          </a>
+        </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse " id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  HOME
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  ABOUT
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  SERVIECS
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  PORTFOLIO
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  CONTACT
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </header>
   );
-}
+};
