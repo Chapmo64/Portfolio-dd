1921fc020694ad7feecdc675e58b4c97f02a70c3
Jashanpreet Singh <141000921+Chapmo64@users.noreply.github.com>
2024-07-09 13:50:38 +0530

    reacted

---
+++ portfolio-dd/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+        <h1>Run</h1>
+        <img src="i8-lunacy://node?id=CdQ5pqyG_02D3tfY6gB0zw" alt="f" />
+    </header>
+  );
+}
