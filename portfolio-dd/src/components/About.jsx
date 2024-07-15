89b0afd09d39cdf339193a942a119258a47bc808
Jashanpreet Singh <141000921+Chapmo64@users.noreply.github.com>
2024-07-15 19:40:22 +0530

    about done

--- portfolio-dd/src/components/About.jsx
+++ portfolio-dd/src/components/About.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export const About = () => {
   return (
     <div className='container-fluid tem'>
-        <h1>JP5INGH</h1>
+        <h1 id='box-title'>JP5INGH</h1>
+        <div className="container-info">
+          <span id='container-tit'>WEB-DEV</span>
+          <span id='container-tit'>GRAPHIC-DESIGNER</span>
+          <div className="tem-contain-info">
+            <p id='info-tit'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum culpa eligendi voluptatum necessitatibus labore pariatur vero sapiente voluptate repudiandae suscipit. Quidem recusandae aperiam quis a tempora nesciunt, reprehenderit voluptates aspernatur.</p>
+          </div>
+        </div>
     </div>
   )
 }
