9b5a00fe628393cf14b7428777e82ceca95747ed
Jashanpreet Singh <141000921+Chapmo64@users.noreply.github.com>
2024-07-15 17:41:49 +0530

    Footer Done

--- portfolio-dd/src/components/Fotter.jsx
+++ portfolio-dd/src/components/Fotter.jsx
@@ -4,37 +4,38 @@ export const Fotter = () => {
   return (
     <footer>
       <div className='foot'>
-        <p>GET IN TOUCH</p>
-        <div className="nav-cont collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#" id="nav-home">
-                  HOME
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  ABOUT
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  SERVIECS
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  PORTFOLIO
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  CONTACT
-                </a>
-              </li>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p id='foot-p'>GET IN TOUCH</p>
+        <div className="fot-link-icon">
+        <i class="fa-brands fa-x-twitter"></i>
+        <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-linkedin"></i>
+        <i class="fa-brands fa-deviantart"></i>
+        <i class="fa-brands fa-github"></i>
+        </div>
+        <div className='fot-link d-flex'>
+            <ul className='fot-link-list'>
+              <li>HOME</li>
+              <li>ABOUT</li>
+              <li>SERVICES</li>
+              <li>PORTFOLIO</li>
+              <li>CONTACT</li>
             </ul>
           </div>
-        <img src="Fotter.png" alt="foot" />
+          <p id='foot-p-2'>Jashanpreet Singh| All Rights Reserved</p>
       </div>
     </footer>
   )
