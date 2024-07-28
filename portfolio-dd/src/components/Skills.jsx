c1f53f54b4e1589512bef92b5b0ac1b8da0e144c
Jashanpreet Singh <141000921+Chapmo64@users.noreply.github.com>
2024-07-28 22:27:15 +0530

    Completed skills section

--- portfolio-dd/src/components/Skills.jsx
+++ portfolio-dd/src/components/Skills.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export const Skills = () => {
   return (
-    <div className='container-fluid tem'
-        style={{backgroundImage: "url(Sec.png)",}}
+    <div className='container-fluid tem1'
+        style={{backgroundImage: "url(skillbg.png)",}}
     >
         <h1 id='box-title-01'>SKILLS</h1>
-        <div className="container-info" >
-          
+        <div className="container-info-skills" >
+          <img src="htmllogo.png" alt="html" />
+          <img src="csslogo.png" alt="css" />
+          <img src="jslogo.png" alt="js" />
+          <img src="reactlogo.png" alt="html" />
+          <img src="pythonlogo.png" alt="html" />
+          <img src="cpplogo.png" alt="html" />
+          <img src="clogo.png" alt="html" />
+          <img src="blenderlogo.png" alt="html" />
+          <img src="dartlogo.png" alt="html" />
+          <img src="lunacylogo.png" alt="html" />
         </div>
     </div>
   )
