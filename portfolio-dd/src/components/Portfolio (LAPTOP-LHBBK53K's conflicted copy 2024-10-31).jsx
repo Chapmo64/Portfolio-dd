01826c88dd70fafb9adc8a62089cfd8a17a2f4c9
Jashanpreet Singh <141000921+Chapmo64@users.noreply.github.com>
2024-12-16 09:14:46 +0530

    noo

---
+++ portfolio-dd/src/components/Portfolio (LAPTOP-LHBBK53K's conflicted copy 2024-10-31).jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import MyCarousel from './MyCarousel';
+
+const Portfolio = () => {
+
+  return(
+    <div className='Csection'>
+      
+      <MyCarousel />
+      <br /><br />
+      <h3>These are the Projects I made in my free time and during the training and online self-study seations</h3>
+      <h4>Technical Projects to learn Whole of Web-Dev</h4>
+      <br />
+    </div>
+  );
+};
+
+export default Portfolio;
